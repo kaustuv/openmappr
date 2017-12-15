e1994021dab065b57c49385e94bd166771511477
bimal1331 <blueice.b13@gmail.com>
2017-12-15 21:14:34 +0530

    feat(docker): trying athena setup without venv WIP

--- athena/run_dev_mode.sh
+++ athena/run_dev_mode.sh
@@ -13,10 +13,10 @@ echo "Starting $NAME"
 
 # activate the virtualenv
 # cd $VENVDIR
-source venv/bin/activate
+# source venv/bin/activate
 
 # cd ..
 # Start your unicorn
 # exec python athena_main.py
-# exec python athena_beanstalkd.py
-exec circusd athena_local.ini
+exec python athena_beanstalkd.py
+# exec circusd athena_local.ini
