081072aaebec6648317c5018b2e8f6d919bad136
Aditya <blackleopard92@gmail.com>
2018-01-29 15:25:59 -0800

    feat(mongo): Get a basic version of mongodb committed to the repo

--- run_docker_mode.sh
+++ run_docker_mode.sh
@@ -4,4 +4,4 @@ NODE_ENV="docker"
 export NODE_ENV
 
 sleep 15
-exec ./node_modules/nodemon/bin/nodemon.js  --debug --watch server server.js
+exec ./node_modules/nodemon/bin/nodemon.js --watch server server.js
